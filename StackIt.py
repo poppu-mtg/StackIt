544cb43cae44d92ffa6c8e060ad9d39825318457
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-03-13 05:33:12 +1100

    Start deduplicating code around get_card_info
    This change deduplicates the code that handles the minor difference between `1 forest` and `1 forest / inv`, which was vital for the implementation of #14

--- StackIt.py
+++ StackIt.py
@@ -1,5 +1,4 @@
-import os, sys, glob
-import re
+import os, sys
 
 #Image manipulation
 from PIL import Image
@@ -17,6 +16,7 @@ from lxml import html
 import requests
 
 import scraper, config
+from globals import Card, specmana
 
 #ensure that mana costs greater than 9 (Kozilek, Emrakul...) aren't misaligned
 adjustcmc = False
@@ -24,7 +24,6 @@ check9 = '0123456'
 
 def GenerateCMC(name, set):
     global adjustcmc
-    name2 = ''.join(e for e in name if e.isalnum())
     diskcost = cost.strip().replace('*', '_')
     lookupCMC = os.path.join('CmcCache', '{cost}.png'.format(cost=diskcost))
     if os.path.exists(lookupCMC):
@@ -84,12 +83,6 @@ ybot = 45.25
 fnt = ImageFont.truetype("beleren-webfonts/belerensmallcaps-bold-webfont.ttf", 14)
 fnt_title = ImageFont.truetype("beleren-webfonts/belerensmallcaps-bold-webfont.ttf", 18)
 
-#create basic landtypes database:
-basics = ["plains","island","swamp","mountain","forest"]
-
-#create a dictionary for non-basic mana:
-specmana = {'0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9', '10': '10', '11': '11', '12': '12', '13': '13', '14': '14', '15': '15', '16': '16', 'X': 'X', 'C': 'C', 'W': 'W', 'U': 'U', 'B': 'B', 'R': 'R', 'G': 'G', 'PW': 'P', 'PU': 'Q', 'PB': 'S', 'PR': 'T', 'PG': 'V', '2W': 'Y', '2U': 'Z', '2B': '@', '2R': 'A', '2G': 'D', 'WU': 'E', 'UB': 'F', 'BR': 'H', 'RG': 'I', 'GW': 'J', 'WB': 'K', 'UR': 'L', 'BG': 'M', 'RW': 'N', 'GU': 'O'}
-
 #check the input format
 isXML = False
 
@@ -141,7 +134,7 @@ isDeckXML = mmap.mmap(decklist1.fileno(), 0, access=mmap.ACCESS_READ)
 if isDeckXML.find('xml') != -1:
     print('Warning - input decklist is in XML format')
     isXML = True
-    
+
 #check for readable content
 if isXML:
 
@@ -223,16 +216,9 @@ if not isXML:
     decklist = open(str(sys.argv[1]), 'r')
 
     for lines in decklist:
-
-        ncount_card = 0
-
         #necessary for appropriate treatment of the missing mana cost of lands
         isitland = 0
         isitspecland = 0
-
-        #reset the new parser
-        set = None
-        scan_part1 = ' '
 
         if lines[0] == '#':
             continue
@@ -258,169 +244,13 @@ if not isXML:
         
         #this step checks whether a specific art is requested by the user - provided via the set name
 
-        if lines.find('/') != -1:
-
-            data = lines.split(" / ")
-
-            #split the info at the first blank space
-            quantity = int(data[0].split(" ",1)[0])
-            name = data[0].split(" ",1)[1]
-
-            if quantity == 0:
-                continue
-
-            set = data[1].split("\n")[0].lower()
-
-            for landtype in basics:
-
-                if name.lower() == landtype:
-
-                    isitland = 1
-
-            if isitland != 1:
-
-                #update the cardname as the string to be looked at in the html code of mtgvault.com - finds both CMC and set name
-                name_sub = name.replace(",","")
-                name_sub = name_sub.replace("'"," ")
-                print name_sub
-
-                cmcsearch = name_sub.replace(" ","+")
-                scansearch = name_sub.replace(" s ","s ")
-                scansearch = scansearch.replace(" ","-")
-                scansearch = scansearch.lower()
-                print cmcsearch,scansearch
-
-                cmcweb = 'http://www.mtgvault.com/cards/search/?q={cmcsearch}&searchtype=name&s={set}'.format(cmcsearch=cmcsearch,set=set)
-
-                cmcpage = requests.get(cmcweb)
-                cmctree = html.fromstring(cmcpage.content)
-
-                scankey = "/card/" + scansearch + '/'
-
-                cmcscan = cmctree.xpath('//a[img[@class="card_image"]]/@href')
-    #            print cmcscan
-                for item in cmcscan:
-    #                print item,ncount_card
-                    if scan_part1 != " ":
-                        continue
-                    if item.find(scankey) == 0:
-                        print "found it:",item
-                        scan_part1 = item.split(scankey)[1]
-                    ncount_card = ncount_card + 1
-
-                cmctext = cmctree.xpath('//div[@class="view-card-center"]/p/text()')
-                print cmctext
-
-                finallist = []
-                for item in cmctext:
-                    if item[-1] == "}":
-                        finallist.append(item)
-                print finallist
-
-                if cmctext[0].find("Land") != -1:
-                    isitspecland = 1
-                else:
-    #                cmc_part1 = str(cmctext[0].split(" {")[1])[:-1]
-                    cmc_part1 = str(finallist[ncount_card-1].split(" {")[1])[:-1]
-                    altcmc = cmc_part1.split("}{")
-                    altcmc = [specmana[x] for x in altcmc]
-                    print name,altcmc
-
-                if isitspecland == 1:
-                    cost = "*\n"
-                else:
-                    cost = "".join(altcmc)+"\n"
-
-            else:
-
-                cost = "*\n"
-
-            print name,set,cost
-
-        else:
-
-            #split the info at the first blank space
-            quantity = int(lines.split(" ",1)[0])
-            name = lines.split(" ",1)[1].strip()
-
-            set = config.Get('cards', name)
-
-            if quantity == 0:
-                continue
-
-            for landtype in basics:
-
-                if name.lower() == landtype:
-
-                    isitland = 1
-
-            if isitland != 1:
-
-                #update the cardname as the string to be looked at in the html code of mtgvault.com - finds both CMC and set name
-                name_sub = name.replace(",","")
-                name_sub = name_sub.replace("'"," ")
-                print name_sub
-
-                cmcsearch = name_sub.replace(" ","+")
-                scansearch = name_sub.replace(" s ","s ")
-                scansearch = scansearch.replace(" ","-")
-                scansearch = scansearch.lower()
-                print cmcsearch,scansearch
-
-                cmcweb = 'http://www.mtgvault.com/cards/search/?q={cmcsearch}&searchtype=name'.format(cmcsearch=cmcsearch)
-
-                cmcpage = requests.get(cmcweb)
-                cmctree = html.fromstring(cmcpage.content)
-
-                scankey = "/card/" + scansearch + '/'
-
-                cmcscan = cmctree.xpath('//a[img[@class="card_image"]]/@href')
-    #            print cmcscan
-                for item in cmcscan:
-    #                print item,ncount_card
-                    if scan_part1 != " ":
-                        continue
-                    if item.find(scankey) == 0:
-                        print "found it:",item
-                        scan_part1 = item.split(scankey)[1]
-                    ncount_card = ncount_card + 1
-                altscan = str(scan_part1.split('/"')[0]).lower()
-
-                set = altscan[:-1]
-
-                cmctext = cmctree.xpath('//div[@class="view-card-center"]/p/text()')
-                print cmctext
-
-                finallist = []
-                for item in cmctext:
-                    if item[-1] == "}":
-                        finallist.append(item)
-                print finallist
-
-                if cmctext[0].find("Land") != -1:
-                    isitspecland = 1
-                elif not '}' in cmctext[0]:
-                    # Lotus Bloom, Evermind, and other costless cards.
-                    altcmc = "**"
-                else:
-    #                cmc_part1 = str(cmctext[0].split(" {")[1])[:-1]
-                    cmc_part1 = str(finallist[ncount_card-1].split(" {")[1])[:-1]
-                    altcmc = cmc_part1.split("}{")
-                    altcmc = [specmana[x] for x in altcmc]
-                    print name,altcmc
-
-                if isitspecland == 1:
-                    cost = "*\n"
-                else:
-                    cost = "".join(altcmc)+"\n"
-                print name,set,cost
-
-            else:
-
-                #all basic lands will be using Unhinged card art
-                if set is None:
-                    set = "uh"
-                cost = "*\n"
+        card = scraper.get_card_info(lines)
+        if card is None:
+            continue
+        name = card.name
+        set = card.set
+        cost = card.cost
+        quantity = card.quantity
 
         if isSideboard == 1:
             if not doSideboard:
@@ -438,7 +268,7 @@ if not isXML:
                     doitFirstSB.append(set)
                     doitFirstSB.append(cost)
                     sizeFirstSB = sizeFirstSB + 1
-        else:    
+        else:
             if cost == "*\n":
                 doitLast.append(quantity)
                 doitLast.append(name)
@@ -798,7 +628,7 @@ else:
 
         cmc = Image.new('RGBA',(16*len(cost), 16))
 
-        lookupCMC = GenerateCMC(name, set)
+        GenerateCMC(name, set)
 
         #place the cropped picture of the current card
         deck.paste(cut, (0,34*nstep))
