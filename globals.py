b06acd25c4f9f197c26c9faf3c13eb8f5a27dbb5
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-04-28 13:02:49 +1000

    Log python version, to aid with debugging submitted log files.

--- globals.py
+++ globals.py
@@ -30,6 +30,7 @@ else:
     globaldir = sys.path[0]
 
 PY3 = sys.version_info > (3, 0)
+print('Running on python ' + sys.version)
 
 # print("DIR={0}".format(localdir))
 CACHE_PATH = os.path.join(globaldir, 'cache')
